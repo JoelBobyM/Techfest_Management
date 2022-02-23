4e566ace0e7bcb13a9f24f36f3a1f833240cf80e
JoelBobyM <joelbobym@gmail.com>
2022-02-23 22:06:08 +0530

    Login View

---
+++ src/techfest/Login.java
@@ -0,0 +1,20 @@
+package techfest;
+
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class Login
+{
+    JFrame f1;
+    JPanel p1,p2;
+    public Login()
+    {
+        f1 = new JFrame("LOGIN");
+        p1 = new JPanel();
+        p2 = new JPanel();
+
+        f1.setSize(300,300);
+        f1.setLayout(null);
+        f1.setDefaultCloseOperation(f1.EXIT_ON_CLOSE);
+    }
+}
