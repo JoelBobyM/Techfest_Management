fa953c83a140bbc572113a2815a29d75cd688923
Haseen-Fairuz <haseenfairuz@gmail.com>
2022-02-27 16:56:22 +0530

    spview

--- src/techfest/Searchparticipant.java
+++ src/techfest/Searchparticipant.java
@@ -1,5 +1,7 @@
 package techfest;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -12,20 +14,36 @@ public class Searchparticipant
     JLabel l1;
     JTextField t1;
     JButton b1;
+    ImageIcon im;
+    BufferedImage img;
+    Image dimg;
     public Searchparticipant()
     {
        f1=new JFrame("SEARCH PARTICIPANT");
-       l1=new JLabel("Participant id:");
+       l1=new JLabel("PARTICIPANT ID:");
        t1=new JTextField(15);
-       b1=new JButton("Enter");
+       try
+        {
+            img = ImageIO.read(new File("/home/haseen/Downloads/search.jpg"));
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        dimg = img.getScaledInstance(1100, 650, Image.SCALE_SMOOTH);
+        im = new ImageIcon(dimg);
+
+        b1 = new JButton(im);
+       
 
        f1.add(l1);
        f1.add(t1);
        f1.add(b1);
 
-       l1.setBounds(30,50,40,60);
+       l1.setBounds(60,70,120,30);
+       t1.setBounds(180,70,170,30);
+       b1.setBounds(100,120,80,30);
 
-       f1.setSize(400,400);
+       f1.setSize(400,300);
        f1.setLayout(null);
        f1.setVisible(true);
        f1.setDefaultCloseOperation(f1.EXIT_ON_CLOSE);
