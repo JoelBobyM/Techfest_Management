f41c729660945b66192587ac4394d75efa6b1aa2
JoelBobyM <joelbobym@gmail.com>
2022-02-28 01:36:08 +0530

    Login View

--- src/techfest/Searchparticipant.java
+++ src/techfest/Searchparticipant.java
@@ -1,36 +1,33 @@
 package techfest;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
+import javax.swing.*;
 import java.awt.*;
 
 public class Searchparticipant
 {
     JFrame f1;
-    JLabel l1,l2;
+    JLabel l1,bg;
     JTextField t1;
     JButton b1;
-    Font fn1;
+    ImageIcon ic;
     public Searchparticipant()
     {
        f1=new JFrame("SEARCH PARTICIPANT");
        l1=new JLabel("PARTICIPANT ID:");
        t1=new JTextField(15);
        b1 = new JButton("ENTER");
-       fn1=new Font("SansSerif", Font.BOLD, 20);
-       JLabel l2=new JLabel(new ImageIcon("/home/haseen/Downloads/search.jpg"));
+       ic = new ImageIcon("/home/joelbobym/Documents/JAVA/Techfest_Management/img-src/search.png");
+       bg = new JLabel("",ic,JLabel.CENTER);
 
-       
-       f1.add(l2);
-       l2.add(l1);
-       l2.add(t1);
-       l2.add(b1);
+       f1.add(l1);
+       f1.add(t1);
+       f1.add(b1);
+       f1.add(bg);
 
        l1.setBounds(60,70,120,30);
        t1.setBounds(180,70,170,30);
        b1.setBounds(100,120,80,30);
+       bg.setBounds(0,0,400,300);
 
        f1.setSize(400,300);
        f1.setLayout(null);
