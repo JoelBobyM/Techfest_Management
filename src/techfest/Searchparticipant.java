6d1ce8419e77de291f7b39c414816eecbd754237
Haseen-Fairuz <haseenfairuz@gmail.com>
2022-02-28 01:24:03 +0530

    sp view

--- src/techfest/Searchparticipant.java
+++ src/techfest/Searchparticipant.java
@@ -21,10 +21,10 @@ public class Searchparticipant
        t1=new JTextField(15);
        b1 = new JButton("ENTER");
        fn1=new Font("SansSerif", Font.BOLD, 20);
-       JLabel l2=new JLabel(new ImageIcon("/home/haseen/Downloads/search.jpg"));
+       ImageIcon icon = new ImageIcon("/home/haseen/Downloads/search.jpg");
+       f1.add(new JLabel(icon));
+       f1.pack();
 
-       
-       f1.add(l2);
        f1.add(l1);
        f1.add(t1);
        f1.add(b1);
