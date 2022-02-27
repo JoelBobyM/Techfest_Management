515dcb52464aa5963247c69d5db0d9558022f88b
Haseen-Fairuz <haseenfairuz@gmail.com>
2022-02-27 15:28:16 +0530

    search participant view

--- src/techfest/Searchparticipant.java
+++ src/techfest/Searchparticipant.java
@@ -1,9 +1,31 @@
 package techfest;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
 class Searchparticipant
 {
+    JFrame f1;
+    JLabel l1;
+    JTextField t1;
+    JButton b1;
     Searchparticipant()
     {
-        njdfsndshfesu
+       f1=new JFrame("SEARCH PARTICIPANT");
+       l1=new JLabel("Participant id:");
+       t1=new JTextField(15);
+       b1=new JButton("Enter");
+
+       f1.add(l1);
+       f1.add(t1);
+       f1.add(b1);
+       
+       f1.setSize(400,400);
+       f1.setLayout(null);
+       f1.setVisible(true);
+       f1.setDefaultCloseOperation(f1.EXIT_ON_CLOSE);
     }
 
 }
