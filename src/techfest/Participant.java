a8bb77393324f60860e93bce658173a7d2227701
Haseen-Fairuz <haseenfairuz@gmail.com>
2022-02-28 23:19:47 +0530

    participant

---
+++ src/techfest/Participant.java
@@ -0,0 +1,13 @@
+package techfest;
+
+import javax.swing.JFrame;
+
+public class Participant {
+    JFrame f1;
+
+    public Participant()
+    {
+       f1=new JFrame("PARTICIPANT");
+       
+    }
+}
