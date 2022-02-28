6a8a597cf0562edde3dbb70b1a1db8266e5ac8e2
Haseen-Fairuz <haseenfairuz@gmail.com>
2022-02-28 23:54:18 +0530

    pp

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Login L = new Login();
-        Searchparticipant S = new Searchparticipant();
+        //Login L = new Login();
+        Participant P = new Participant();
     }
 }
