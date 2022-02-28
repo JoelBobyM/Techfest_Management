ff5e8f0fe9f60da186f30e67008d5b2441659be1
Haseen-Fairuz <haseenfairuz@gmail.com>
2022-02-28 22:42:25 +0530

    Merge branch 'master' of https://github.com/JoelBobyM/Techfest_Management

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ public class Main
 {
     public static void main(String[] args)
     {
-        //Login L = new Login();
-        Searchparticipant S = new Searchparticipant();
+        Login L = new Login();
     }
 }
